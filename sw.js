4b532b139af4aeb1001095e48ec8bc3ca656270e
delsonbon-png <delsonbon@gmail.com>
2026-03-22 18:43:57 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'sonic-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './manifest.json',
+  './icon.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
